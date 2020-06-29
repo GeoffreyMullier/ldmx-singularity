28ff928fc1db3f98a5c1716d2b2d67d58562d181
GeoffreyMullier <6888308+GeoffreyMullier@users.noreply.github.com>
2020-06-30 01:19:38 +0200

    First Commit

---
+++ src/checkout.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/LDMX-Software/geant4.git ldmx-geant4
+
+# those are currently privat repos that requires credentials
+
+git clone https://github.com/LDMXAnalysis/ldmx-sw.git ldmx-git
+git clone https://github.com/LDMXAnalysis/MagFieldMap MagFieldMap
+
