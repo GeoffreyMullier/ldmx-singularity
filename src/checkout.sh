486d36c3d5424a2f2b84404dfd4452906ef7513b
Geoffrey Mullier <geoffrey.mullier@cern.ch>
2020-07-03 02:33:47 +0200

    Fixing recursive on checking out the repos

--- src/checkout.sh
+++ src/checkout.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/LDMX-Software/geant4.git ldmx-geant4
+git clone --recursive https://github.com/LDMX-Software/geant4.git ldmx-geant4
 
 # those are currently privat repos that requires credentials
 
-git clone https://github.com/LDMXAnalysis/ldmx-sw.git ldmx-git
-git clone https://github.com/LDMXAnalysis/MagFieldMap MagFieldMap
+git clone --recursive https://github.com/LDMXAnalysis/ldmx-sw.git ldmx-git
+git clone --recursive https://github.com/LDMXAnalysis/MagFieldMap MagFieldMap
 
