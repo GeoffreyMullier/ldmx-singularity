d6cedff57e9fdf3def6c60bb95cfcc42b3623251
Geoffrey Mullier <geoffrey.mullier@cern.ch>
2020-07-08 14:41:16 +0200

    fixing git checkouts

--- src/checkout.sh
+++ src/checkout.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf ldmx-geant4
+rm -rf ldmx-git
+rm -rf MagFieldMap
+
 git clone --recursive https://github.com/LDMX-Software/geant4.git ldmx-geant4
 
 # those are currently privat repos that requires credentials
